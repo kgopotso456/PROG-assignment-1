7aa637da7c76672007e53ccd46656f96db500e19
kgopotso456 <kgopotsongwato5@gmail.com>
2026-03-24 15:34:55 +0200

    Update userSysten.java
    added explanations for the user to see when entering their password an d username and added password complexity

--- userSysten.java
+++ userSysten.java
@@ -17,12 +17,32 @@ public class userSystem
 		//********Registration************
 		System.out.println("*****Registration*****");
 		String userName;
+		System.out.println("Your username must contain an underscore (_) and is no more than five characters long");
 		System.out.println("Create a username: ");
 		userName = sc.nextLine();
 		
 		String password;
+		System.out.println("Your password must be at least eight charaters long, contain a capital letter(s),a number(s), and a speacial character(s).");
 		System.out.println("Create a password: ");
 		password = sc.nextLine();
+		
+		boolean hasCap = false;
+		boolean hasNum = false;
+		boolean hasSpec = false;
+		
+		for(int i = 0; i < password.length(); i++){
+		    char ch = password.charAt(i);
+		    
+		    if(Character.isUpperCase(ch)){
+		        hasCap = true;
+		    }
+		    if(Character.isDigit(ch)){
+		        hasNum = true;    
+		    }
+		    if(!Character.isLetterOrDigit(ch)){
+		        hasSpec = true;
+		    }
+		}
 		
 		String cellPhoneNumber;
 		System.out.println("Enter your cell phone number: ");
@@ -33,7 +53,13 @@ public class userSystem
 		System.out.println("Username successfully captured ");
 		}
 	else{
-	    System.out.println("Username is not correctly formatte;please ensure that your username contains an underscore and it is not more than 5 characters in length");
+	    System.out.println("Username is not correctly formatte;please ensure that your username contains an underscore and it is not more than 5 characters in length.");
 	}
+	  if(hasCap && hasNum && hasSpec && password.length() >= 8){
+	    System.out.println("Password successfully captured ");  
+	  }
+	  else{
+	      System.out.println("password is not correctly formatte; please ensure that your password conntains at least eight charaters, a number, and a special charater.");
+	  }
 	}
 }
